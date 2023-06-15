a6c44510330a3c7a48f947ed7d7fdc14769aca80
DomeAnchundia24 <135488626+DomeAnchundia24@users.noreply.github.com>
2023-06-15 16:56:43 -0500

    Add files via upload

---
+++ Cuenta_Corriente.py
@@ -0,0 +1,33 @@
+from Cuenta_Bancaria import Cuenta_Bancaria
+
+
+class Cuenta_corriente (Cuenta_Bancaria):
+
+    def __init__(self, numero_cuenta=None, propietario=None, saldo: float = 0, tiene_cheque: bool = False):
+        self._tiene_cheque = tiene_cheque
+        super().__init__(numero_cuenta=numero_cuenta, propietario=propietario, balance=saldo)
+
+    @property
+    def tiene_cheque(self):
+        return self._tiene_cheque
+
+    @tiene_cheque.setter
+    def tiene_cheque(self, tiene_cheque):
+        self._tiene_cheque = tiene_cheque
+
+    def pagar_cheque(self):
+        if self._tiene_cheque:
+            self._balance -= self._balance
+        return self._balance
+
+
+if __name__ == '__main__':
+    Cuentas_corrientes = Cuenta_corriente('1234567890', 'Domenica', 250, True)
+
+    Cuentas_corrientes.mostrar_balance()
+    Cuentas_corrientes.credito(1400)
+
+    Cuentas_corrientes.mostrar_balance()
+    Cuentas_corrientes.debito(30)
+
+    print(Cuentas_corrientes)
